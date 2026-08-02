c089ff0b97080796bf8454e353ccf3178890be59
kennedychinazam <kennedychinazam@gmail.com>
2026-08-02 12:42:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MatchMind PRO service worker — offline app shell
-const CACHE = 'matchmind-v64';
+const CACHE = 'matchmind-v65';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
